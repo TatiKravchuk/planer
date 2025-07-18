a8a330c91a3e0b4ff8a7d3f554f4067ab0b8ec6d
Tatiana <tati_piv@mail.ru>
2025-07-18 21:42:27 +0300

    correct api

--- src/components/header/weatherBadge/weatherBadge.js
+++ src/components/header/weatherBadge/weatherBadge.js
@@ -6,7 +6,7 @@ function WeatherBadge({ onClick, className }) {
   const [text, setText] = useState("Загрузка...");
 
   useEffect(() => {
-    fetch("/json")
+    fetch("https://ipapi.co/json/")
       .then(res => res.json())
       .then(data => {
         const detectedCity = data.city || "";
